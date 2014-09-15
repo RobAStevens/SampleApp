7df672ecd6ab92023d16691bad9f5d3f3fd7d084
Rob Stevens <rob.a.stevens@gmail.com>
2014-09-15 01:41:54 +0100

    Added localisation of text and storyboards Added image resources for company logo iPhone storyboards fairly advanced, iPad storyboards has first view only and thus “Continue” button is prevented from advancing to the next page

--- SampleApp/SampleApp/ViewController.h
+++ SampleApp/SampleApp/ViewController.h
@@ -8,6 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
+
+// Action for "Continue" button at base of first page
+- (IBAction)ContinueToPage2:(id)sender;
+
+// If we tap anywhere other than in a control, we'll close any edit object
+- (IBAction)tapEmpty:(id)sender;
+
+// Touching the date picker should also end any textfield editing
+- (IBAction)touchedDatePicker:(id)sender;
+
+// We maintain a property to identify the active locale
+enum KnownLocales {
+	LOCALE_NONE,		// If we come across an unexpected locale, we tag it as none and methods can decide their own default actions
+	LOCALE_UK,
+	LOCALE_DE,
+	LOCALE_US
+};
+@property (nonatomic) int locale;
+
+// Referencing outlets for the 4 textFields
+@property (weak, nonatomic) IBOutlet UITextField *txtFirstName;
+@property (weak, nonatomic) IBOutlet UITextField *txtLastName;
+@property (weak, nonatomic) IBOutlet UITextField *txtEmail;
+@property (weak, nonatomic) IBOutlet UITextField *txtPassword;
+
+// The date picker
+@property (weak, nonatomic) IBOutlet UIDatePicker *pkrDate;
 
 @end
--- SampleApp/SampleApp/ViewController.m
+++ SampleApp/SampleApp/ViewController.m
@@ -14,16 +14,144 @@
 
 @implementation ViewController
 
+// -------------------------------------------------------------
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	// We're going to use the system locale to decide which UI elements to present to the user
+	// We're only interested in the countries the app is going to be released in (UK, US and Germany), so we check specifically
+	// for those. We're not concerned by languages at this point since text will be handled by the localized strings, we just need
+	// the correct region
+	
+	NSString *countryCode = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+
+	// Log the country code we found for debugging
+	NSLog(@"Country code %@", countryCode);
+	
+	// We'll default to the UK signup behaviour if we don't get a recognisable locale (we could also refuse the sign up)
+	if ([countryCode compare:@"UK"] == NSOrderedSame)
+		[self setLocale:LOCALE_UK];
+	else if ([countryCode compare:@"US"] == NSOrderedSame)
+		[self setLocale:LOCALE_US];
+	else if ([countryCode compare:@"DE"] == NSOrderedSame)
+		[self setLocale:LOCALE_DE];
+	else
+		[self setLocale:LOCALE_NONE];
 }
 
+// -------------------------------------------------------------
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+// -------------------------------------------------------------
+// When we hit return on any of the text fields we'd like them to resign the first responder
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+	[textField resignFirstResponder];
+	return YES;
+}
+
+// -------------------------------------------------------------
+// If we tap anywhere other than in a control, we'll close any edit object
+- (IBAction)tapEmpty:(id)sender
+{
+	[self.view endEditing:YES];
+}
+
+// -------------------------------------------------------------
+
+- (IBAction)touchedDatePicker:(id)sender {
+	[self.view endEditing:YES];
+}
+
+// -------------------------------------------------------------
+// Action associated with the "Continue" button on the first page of the app
+// We want to validate the data entered before progressing to the next step
+- (IBAction)ContinueToPage2:(id)sender {
+	
+	// Validate each textfield for empty fields
+	if ([[self.txtFirstName text] isEqualToString:@""])
+	{
+		[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Incomplete Form", nil)
+									message:NSLocalizedString(@"Please enter your first name", nil)
+								   delegate:nil
+						  cancelButtonTitle:NSLocalizedString(@"OK", nil)
+						  otherButtonTitles:nil]
+		 show];
+		return;
+	}
+	
+	if ([[self.txtLastName text] isEqualToString:@""])
+	{
+		[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Incomplete Form", nil)
+									message:NSLocalizedString(@"Please enter your last name", nil)
+								   delegate:nil
+						  cancelButtonTitle:NSLocalizedString(@"OK", nil)
+						  otherButtonTitles:nil]
+		 show];
+		return;
+	}
+	
+	// For the user's email, we also want to check that the text provided is in the form of an email address
+	if ([[self.txtEmail text] isEqualToString:@""] || ![self validateEmail:[self.txtEmail text]])
+	{
+		[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Incomplete Form", nil)
+									message:NSLocalizedString(@"Please enter a valid email address", nil)
+								   delegate:nil
+						  cancelButtonTitle:NSLocalizedString(@"OK", nil)
+						  otherButtonTitles:nil]
+		 show];
+		return;
+	}
+	
+	// Make sure we have a password
+	if ([[self.txtPassword text] isEqualToString:@""])
+	{
+		[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Incomplete Form", nil)
+									message:NSLocalizedString(@"Please enter a password", nil)
+								   delegate:nil
+						  cancelButtonTitle:NSLocalizedString(@"OK", nil)
+						  otherButtonTitles:nil]
+		 show];
+		return;
+	}
+
+	// Copy all the data gathered so far into an NSDictionary for later passing to the server
+	
+	
+	// Here we manually switch which view we want to display next by checking the locale
+	// We'll default to the UK signup behaviour if we don't have a recognisable locale (we could also refuse the sign up)
+	NSString* segue = @"UK_SignUp";
+	if (self.locale == LOCALE_US)
+		segue = @"US_SignUp";
+	else if (self.locale == LOCALE_DE)
+		segue = @"DE_SignUp";
+	
+	// DEBUG: For the moment this will crash on iPad since the segues are undefined, so we prevent it
+	if ([[UIDevice currentDevice] userInterfaceIdiom] != UIUserInterfaceIdiomPad)
+		[self performSegueWithIdentifier:segue sender:self];
+}
+
+// --------------------------------------------------------------------------
+// Use Regex to validate the formating of the email entered
+//
+// Returns NO if the email address appears invalid, YES if the address appears to be valid
+
+-(BOOL) validateEmail:(NSString*)emailString {
+	// This is not the best ever regex for checking email formatting, but we're just trying to pick up user error rather than policing what they're providing us, so it is a good compromise
+    NSString *regExPattern = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+	
+    NSRegularExpression *regEx = [[NSRegularExpression alloc] initWithPattern:regExPattern options:NSRegularExpressionCaseInsensitive error:nil];
+	
+    NSUInteger regExMatches = [regEx numberOfMatchesInString:emailString options:0 range:NSMakeRange(0, [emailString length])];
+	
+    return (regExMatches == 0) ? NO : YES;
+}
+
 
 @end
